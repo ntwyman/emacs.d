126d300c1dd496567e8732974ddf866b8d75b9c3
Nick Twyman <ntwyman@morseproject.com>
2013-06-26 21:05:46 -0700

    Added ack

--- init.el
+++ init.el
@@ -1,2 +1,39 @@
+; server start for emacs-client
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+; Startup screen drives me nuts
 (setq inhibit-startup-screen t)
-(set-default-font "Liberation Mono-14")
+
+; Local stuff
+(add-to-list 'load-path "~/.emacs.d")
+
+; Ack!
+(require 'ack-and-a-half)
+;; Create shorter aliases
+(defalias 'ack 'ack-and-a-half)
+(defalias 'ack-same 'ack-and-a-half-same)
+(defalias 'ack-find-file 'ack-and-a-half-find-file)
+(defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
+
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(load-theme 'monokai t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("68769179097d800e415631967544f8b2001dae07972939446e21438b1010748c" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "apple" :family "Cousine")))))
